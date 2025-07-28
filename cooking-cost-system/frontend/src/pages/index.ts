42263520251160e172dd0672eff1ec01f89110dd
nuu1729 <haya.take23@icloud.com>
2025-07-28 14:45:45 +0900

    Create index.ts

---
+++ cooking-cost-system/frontend/src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as MainPage } from './MainPage';
+export { default as AdminPage } from './AdminPage';
+export { default as ReportsPage } from './ReportsPage';
+export { default as SettingsPage } from './SettingsPage';
+export { default as NotFoundPage } from './NotFoundPage';
