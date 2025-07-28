a25f0d72abb760df71916d7efffe2a5df2ee7429
nuu1729 <haya.take23@icloud.com>
2025-07-28 14:37:36 +0900

    Create index.ts

---
+++ cooking-cost-system/frontend/src/components/index.ts
@@ -0,0 +1,19 @@
+// Common Components
+export { default as Layout } from './common/Layout';
+export { default as LoadingSpinner } from './common/LoadingSpinner';
+export { default as Modal } from './common/Modal';
+export { default as ErrorFallback } from './common/ErrorFallback';
+export { default as AddIngredientModal } from './common/AddIngredientModal';
+export { default as DishBuilder } from './common/DishBuilder';
+
+// Ingredient Components
+export { IngredientCard } from './ingredients/IngredientCard';
+
+// Dish Components
+export { DishCard } from './dishes/DishCard';
+
+// Completed Food Components
+export { CompletedFoodCard } from './completedFoods/CompletedFoodCard';
+
+// Report Components
+export { StatsCard } from './reports/StatsCard';
